88b5e200132d24aa7809e34340d29e14b71cfce6
imnites <nitesh110897@gmail.com>
2024-03-22 22:58:18 +0530

    working model

--- server/model.py
+++ server/model.py
@@ -3,20 +3,19 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split  
 import os
 import cv2
-# from google.colab.patches import cv2_imshow
-# import matplotlib.pyplot as plt
-# from tensorflow.keras.layers import Dropout
-# from tensorflow.keras.layers import Flatten,BatchNormalization
-# from tensorflow.keras.layers import Dense, MaxPooling2D,Conv2D
-# from tensorflow.keras.layers import Input,Activation,Add
-# from tensorflow.keras.models import Model
-# from tensorflow.keras.regularizers import l2
-# from tensorflow.keras.optimizers import Adam
-# from tensorflow.keras.callbacks import ModelCheckpoint
-# import pandas as pd
-# from keras.models import Sequential,load_model,Model
-# from keras.layers import Conv2D,MaxPool2D,Dense,Dropout,BatchNormalization,Flatten,Input
-# from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten,BatchNormalization
+from tensorflow.keras.layers import Dense, MaxPooling2D,Conv2D
+from tensorflow.keras.layers import Input,Activation,Add
+from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+import pandas as pd
+from keras.models import Sequential,load_model,Model
+from keras.layers import Conv2D,MaxPool2D,Dense,Dropout,BatchNormalization,Flatten,Input
+from sklearn.model_selection import train_test_split
 
 
 dataset_folder='./input/CK+48'
@@ -59,3 +58,64 @@ labels_y_encoded = tf.keras.utils.to_categorical(labels_y,num_classes=num_of_cla
 
 # Split into 75:25 train and test
 X_train, X_test, Y_train, Y_test= train_test_split(images_x, labels_y_encoded,test_size=0.25, random_state=10)
+
+
+# CNN Architecture
+input = Input(shape = (48,48,1))
+conv1 = Conv2D(32,(3, 3), padding = 'same', strides=(1, 1), kernel_regularizer=l2(0.001))(input)
+conv1 = Dropout(0.1)(conv1)
+conv1 = Activation('relu')(conv1)
+pool1 = MaxPooling2D(pool_size = (2,2)) (conv1)
+conv2 = Conv2D(64,(3, 3), padding = 'same', strides=(1, 1), kernel_regularizer=l2(0.001))(pool1)
+conv2 = Dropout(0.1)(conv2)
+conv2 = Activation('relu')(conv2)
+pool2 = MaxPooling2D(pool_size = (2,2)) (conv2)
+conv3 = Conv2D(128,(3, 3), padding = 'same', strides=(1, 1), kernel_regularizer=l2(0.001))(pool2)
+conv3 = Dropout(0.1)(conv3)
+conv3 = Activation('relu')(conv3)
+pool3 = MaxPooling2D(pool_size = (2,2)) (conv3)
+conv4 = Conv2D(256,(3, 3), padding = 'same', strides=(1, 1), kernel_regularizer=l2(0.001))(pool3)
+conv4 = Dropout(0.1)(conv4)
+conv4 = Activation('relu')(conv4)
+pool4 = MaxPooling2D(pool_size = (2,2)) (conv4)
+flatten = Flatten()(pool4)
+dense_1 = Dense(128,activation='relu')(flatten)
+drop_1 = Dropout(0.2)(dense_1)
+output = Dense(7,activation="sigmoid")(drop_1)
+
+# Model compile
+model = Model(inputs=input,outputs=output)
+model.compile(optimizer="adam", loss=["categorical_crossentropy"], metrics=['accuracy'])
+model.summary()
+
+# Configure Model Checkpoint
+fle_s='./output/emotion_model.keras'
+checkpointer = ModelCheckpoint(fle_s, monitor='loss',verbose=1,save_best_only=True,
+                               save_weights_only=False, mode='auto',save_freq='epoch')
+callback_list=[checkpointer]
+
+
+save = model.fit(X_train,Y_train,batch_size=32,validation_data=(X_test,Y_test),epochs=50,callbacks=[callback_list])
+
+
+train_loss = save.history['loss']
+test_loss = save.history['val_loss']
+train_accuracy = save.history['accuracy']
+test_accuracy = save.history['val_accuracy']
+
+# Plotting a line chart to visualize the loss and accuracy values by epochs.
+fig, ax = plt.subplots(ncols=2, figsize=(15,7))
+ax = ax.ravel()
+ax[0].plot(train_loss, label='Train Loss', color='royalblue', marker='o', markersize=5)
+ax[0].plot(test_loss, label='Test Loss', color = 'orangered', marker='o', markersize=5)
+ax[0].set_xlabel('Epochs', fontsize=14)
+ax[0].set_ylabel('Categorical Crossentropy', fontsize=14)
+ax[0].legend(fontsize=14)
+ax[0].tick_params(axis='both', labelsize=12)
+ax[1].plot(train_accuracy, label='Train Accuracy', color='royalblue', marker='o', markersize=5)
+ax[1].plot(test_accuracy, label='Test Accuracy', color='orangered', marker='o', markersize=5)
+ax[1].set_xlabel('Epochs', fontsize=14)
+ax[1].set_ylabel('Accuracy', fontsize=14)
+ax[1].legend(fontsize=14)
+ax[1].tick_params(axis='both', labelsize=12)
+fig.suptitle(x=0.5, y=0.92, t="Lineplots showing loss and accuracy of CNN model by epochs", fontsize=16)
