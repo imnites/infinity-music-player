9d1183c450daaac4386206bbfbbe0fefab8a7df3
imnites <nitesh110897@gmail.com>
2024-03-23 12:27:33 +0530

    server input and output fixed

--- server/predict_result.py
+++ server/predict_result.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 import tensorflow as tf
+import base64
+from PIL import Image
+import io
+import codecs
 
 
 emotion_labels = ['anger', 'contempt', 'disgust', 'fear', 'happy', 'sadness', 'surprise']
@@ -9,6 +13,21 @@ model = tf.keras.models.load_model('./output/emotion_model_pretrained.keras')
 
 def get_emotion_predictions(face_img):
     face_img = cv2.imread(face_img, cv2.COLOR_BGR2GRAY)
+    face_img = cv2.resize(face_img, (48, 48))
+    face_img = np.expand_dims(face_img, axis=0)
+    face_img = face_img / 255.0  # Normalize the image
+
+    emotion_preds = model.predict(face_img)
+    prediction_index = emotion_preds[0].argmax()
+
+    percentage = emotion_preds[0][prediction_index]
+    label = emotion_labels[prediction_index]
+    return [label, percentage*100]
+
+def get_emotion_predictions_from_base64_image(base64_string):
+    encoded_data = base64_string.split(',')[1]
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    face_img = cv2.imdecode(nparr, cv2.COLOR_BGR2GRAY)
     face_img = cv2.resize(face_img, (48, 48))
     face_img = np.expand_dims(face_img, axis=0)
     face_img = face_img / 255.0  # Normalize the image
--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import json
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from predict_result import get_emotion_predictions_from_base64_image;
 
 hostName = "localhost"
 serverPort = 8080
@@ -15,6 +17,21 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        jsonBody = json.loads(post_body)
+        base64String = jsonBody['base64']
+        label, percentage = get_emotion_predictions_from_base64_image(base64String)
+        data = {}
+        data['label'] = label
+        data['percentage'] = percentage
+        response = json.dumps(data)
+        self.wfile.write(bytes(response, "utf8"))
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
