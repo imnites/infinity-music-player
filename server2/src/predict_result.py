0b496acbdda226807ec2e444fdd9b8812dfb1a7d
imnites <nitesh110897@gmail.com>
2024-04-09 03:37:24 +0530

    predict_result and server

---
+++ server2/src/predict_result.py
@@ -0,0 +1,61 @@
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+import base64
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+model.load_weights('model.h5')
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+def get_emotion_predictions_from_base64_image(base64_string):
+    encoded_data = base64_string.split(',')[1]
+    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+    face_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    try:
+        face_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
+    except:
+        pass
+
+    faces = facecasc.detectMultiScale(face_img,scaleFactor=1.3, minNeighbors=5)
+
+    labels = []
+
+    for (x, y, w, h) in faces:
+        roi_gray = face_img[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = model.predict(cropped_img)
+        maxindex = int(np.argmax(prediction))
+        labels.append(emotion_dict[maxindex])
+
+    return labels
---
+++ server2/src/server.py
@@ -0,0 +1,54 @@
+import time
+import json
+
+from http.server import CGIHTTPRequestHandler, HTTPServer
+from predict_result import get_emotion_predictions_from_base64_image
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(CGIHTTPRequestHandler):
+    def do_Headers(self):
+        self.send_header('Content-Type','application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Request-Method', 'GET, POST, PUT')
+        self.send_header('Access-Control-Allow-Credentials', 'true')
+        self.send_header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+        self.end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.do_Headers()
+       
+
+    def do_GET(self):
+        self.send_response(200)
+        self.do_Headers()
+        self.wfile.write(bytes('ok', "utf8"))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.do_Headers()
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        jsonBody = json.loads(post_body)
+        base64String = jsonBody['base64']
+        result = get_emotion_predictions_from_base64_image(base64String)
+        if len(result) == 0:
+            return self.wfile.write(bytes('{}', "utf8"))
+        data = {}
+        data['label'] = result[0]
+        response = json.dumps(data)
+        self.wfile.write(bytes(response, "utf8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
